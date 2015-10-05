22a5d313c72d4e9b86712bb5c49c06526122e7ce
Naomi Rosenberg <naomirosenberguk@gmail.com>
2015-10-05 14:15:13 +0100

    Added alternative bullets component

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-pres
 A very very simple presentation engine written in React, complete with React-related content
+
+# To install
+npm install 
+sudo npm install -g webpack-dev-server
+
+# To run, enabling hot-reloading
+webpack-dev-server --hot --inline  
--- index.js
+++ index.js
@@ -9,20 +9,19 @@ var Presentation = React.createClass({
     return (
       <div>
         <Slide {...slides[this.state.slideNum]} />
-        {this.state.slideNum > 0 &&
-          <button onClick={this.prev}>Previous</button>
-        }
-        {this.state.slideNum < slides.length - 1 &&
-          <button onClick={this.next}>Next</button>
-        }
       </div>
     );
   },
-  next() {
-    this.setState({slideNum: this.state.slideNum + 1});
+  componentDidMount() {
+    window.addEventListener("keydown", this.changeSlide);
   },
-  prev() {
-    this.setState({slideNum: this.state.slideNum - 1});
+  changeSlide(e) {
+    if (e.which ==  37 && this.state.slideNum > 0) {
+      return this.setState({slideNum: this.state.slideNum - 1});
+    }
+    if (e.which ==  39 && this.state.slideNum < slides.length - 1) {
+      return this.setState({slideNum: this.state.slideNum + 1});
+    }
   }
 });
 
@@ -31,16 +30,42 @@ var Slide = React.createClass({
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <img src={this.props.img} />
-        <ul>
-          {this.points()}
-        </ul>
+        <img style={{float: 'left'}} src={this.props.img} width={'200px'} />
+        <Bullets bullets={this.props.bullets} />
       </div>
     );
-  },
-  points() {
-    return (this.props.points.map((point, i) => (<li key={i}>{point}</li>)));
   }
 })
+
+var SimpleBullets = React.createClass({
+  render() {
+    return (
+      <ul style={{float: 'left'}}>
+        {this.props.bullets.map((bullet, i) => (<li key={i}>{bullet}</li>))}
+      </ul>
+    );
+  }
+});
+
+var Bullets = React.createClass({
+  getInitialState() {
+    return {numBullets: 0};
+  },
+  render() {
+    return (
+      <ul style={{float: 'left'}}>
+        {this.props.bullets.slice(0, this.state.numBullets).map((bullet, i) => (<li key={i}>{bullet}</li>))}
+      </ul>
+    );
+  },
+  componentDidMount() {
+    window.addEventListener("keydown", this.addBullet);
+  },
+  addBullet(e) {
+    if (e.which == 40) {
+      return this.setState({numBullets: this.state.numBullets + 1})
+    }
+  }
+});
 
 React.render(<Presentation {...slides} />, document.getElementById('main'))
