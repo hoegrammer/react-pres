c023fec04421fb5ffe35cab985ccea5156cf0bdf
Naomi Rosenberg <naomirosenberguk@gmail.com>
2015-10-05 12:13:23 +0100

    Slides in separate file

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var React = require('react');
+var slides = require('./slides');
 
 var Presentation = React.createClass({
-  displayName: 'Presentation',
+  getInitialState() {
+    return {slideNum: 0};
+  },
   render() {
-    return (<div></div>);
+    return (
+      <div>
+        <Slide {...slides[this.state.slideNum]} />
+        {this.state.slideNum > 0 &&
+          <button onClick={this.prev}>Previous</button>
+        }
+        {this.state.slideNum < slides.length - 1 &&
+          <button onClick={this.next}>Next</button>
+        }
+      </div>
+    );
+  },
+  next() {
+    this.setState({slideNum: this.state.slideNum + 1});
+  },
+  prev() {
+    this.setState({slideNum: this.state.slideNum - 1});
   }
 });
 
-React.render(<Presentation />, document.getElementById('main'))
+var Slide = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <img src={this.props.img} />
+        <ul>
+          {this.points()}
+        </ul>
+      </div>
+    );
+  },
+  points() {
+    return (this.props.points.map((point, i) => (<li key={i}>{point}</li>)));
+  }
+})
+
+React.render(<Presentation {...slides} />, document.getElementById('main'))
---
+++ slides.js
@@ -0,0 +1,19 @@
+module.exports = [{
+  title: 'First',
+  points: [
+    'Separation of concerns',
+    'Easy to understand'
+  ]
+},{
+  title: 'Second',
+  points: [
+    'Separation of concerns',
+    'Easy to understand'
+  ]
+},{
+  title: 'Third',
+  points: [
+    'Separation of concerns',
+    'Easy to understand'
+  ]
+}];
