0ffab389a4a73a59538fe2c7321b4cc4ec354b8e
Naomi Rosenberg <naomirosenberguk@gmail.com>
2015-10-05 11:45:47 +0100

    Config working - hot reloading

---
+++ index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var Presentation = React.createClass({
+  displayName: 'Presentation',
+  render() {
+    return (<div></div>);
+  }
+});
+
+React.render(<Presentation />, document.getElementById('main'))
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ }
+        ]
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+    }
+};
